0815c4e6aa24d84b27b254472d188b9fe6f9090a
Seiji Sawayanagi <mostlyfine@gmail.com>
2019-10-25 23:59:28 +0900

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path')
+
+module.exports = {
+  mode: 'development',
+  entry: path.resolve(__dirname, 'src', 'index.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: [
+          { loader: 'babel-loader' }
+        ]
+      }
+    ]
+  }
+};
