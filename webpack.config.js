8f64d3e41fdbd65bf4e0a1526f677aee17ef1ad9
Seiji Sawayanagi <sawayanagi@1101.com>
2020-03-03 15:56:34 +0900

    Add dev server.

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,7 @@ module.exports = {
   entry: path.resolve(__dirname, 'src', 'index.js'),
   output: {
     path: path.resolve(__dirname, 'dist'),
+    publicPath: '/js/',
     filename: 'bundle.js'
   },
   module: {
